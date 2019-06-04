435485c0c1ef0de5535fd756428167b0738aa6e6
duongdinhnghia1999 <49018272+duongdinhnghia1999@users.noreply.github.com>
2019-06-04 15:12:24 +0700

    Add files via upload

--- 11.py
+++ 11.py
@@ -1,6 +1,6 @@
 import numpy as np
 data_type = [('name', 'S15'), ('class', int), ('height', float)]
-students_details = [('James', 5, 48.5), ('Nail', 6, 52.5), ('Paul', 5, 42.10), ('Pit', 5, 40.11), ('Nam', 7, 163)]
+students_details = [('James', 5, 48.5), ('Nail', 6, 52.5), ('Paul', 5, 42.10), ('Pit', 5, 40.11), ('Nghia', 7, 163)]
 students = np.array(students_details, dtype = data_type)
 print("Mảng gốc: ")
 print(students)
